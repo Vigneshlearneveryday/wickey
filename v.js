033be5c02b2645dca8af9c2d3c350d7d1578bb89
Vigneshlearneveryday <nauticalvigneshmenon@gmail.com>
2024-05-15 08:36:37 +0530

    update v.js

--- v.js
+++ v.js
@@ -1,10 +1,10 @@
 console.log(`New World`);
 
 let x = (` hello Elan `);
-let z = (`King of this X`)
+let z = (`King of this X World`);
 console.log(x+z);
 
-let a = (`peace`)
-let b = (`focus`)
+let a = (`peace`);
+let b = (`focus`);
 console.log(a+b);
 
